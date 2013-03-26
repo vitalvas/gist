44cf44f41a2c1f73b79a01e96cdb9a5a749e536f
Vitaliy Vasilenko <me@vitalvas.com>
2013-03-26 08:27:21 +0200

    fix url update file

--- freebsd/bruteblock/update_filter.sh
+++ freebsd/bruteblock/update_filter.sh
@@ -4,6 +4,6 @@ for conf in ssh asterisk
 do
     if [ -f "/usr/local/etc/bruteblock/$conf.conf" ]
     then
-	curl -s https://raw.github.com/vitalvas/freebsd-auto/master/bruteblock/filters/$conf.conf > /usr/local/etc/bruteblock/$conf.conf
+	curl -s https://raw.github.com/vitalvas/gist/master/freebsd/bruteblock/filters/$conf.conf > /usr/local/etc/bruteblock/$conf.conf
     fi
 done
