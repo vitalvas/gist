bcc66f9618b8a9656623daf01a12ecf06991bb20
Vitaliy Vasilenko <vvitalvas@yandex.ru>
2013-04-02 18:09:23 +0300

    Update debian_after_install.sh

--- linux/debian_after_install.sh
+++ linux/debian_after_install.sh
@@ -12,9 +12,9 @@ echo 'LC_ALL=en_US.UTF-8' >> /etc/default/locale
 # timezone
 dpkg-reconfigure tzdata
 
-curl https://raw.github.com/vitalvas/linux-work/master/custom/.bashrc >> ~/.bashrc
+curl https://raw.github.com/vitalvas/gist/master/linux/custom/.bashrc >> ~/.bashrc
 if [ -f /home/vitalvas/.bashrc ];
 then
-  curl https://raw.github.com/vitalvas/linux-work/master/custom/.bashrc >> /home/vitalvas/.bashrc
+  curl https://raw.github.com/vitalvas/gist/master/linux/custom/.bashrc >> /home/vitalvas/.bashrc
 fi
 
