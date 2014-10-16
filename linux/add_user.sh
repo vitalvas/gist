15b5bb1641a8d616e52e11fd0adfc1834a5d7c79
Vitaliy Vasilenko <vvitalvas@yandex.ru>
2014-10-16 23:31:52 +0300

    Update add_user.sh

--- linux/add_user.sh
+++ linux/add_user.sh
@@ -4,3 +4,5 @@
 /usr/bin/env mkdir /home/vitalvas/.ssh
 /usr/bin/env wget --no-check-certificate -q https://github.com/vitalvas.keys -O /home/vitalvas/.ssh/authorized_keys
 /usr/bin/env chown -R vitalvas:vitalvas /home/vitalvas/.ssh
+
+/usr/bin/env echo "vitalvas    ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
